648ddfc9effec8871f191b375ae22b5de7471840
= <=>
2012-01-27 02:07:04 -0600

    Primera versión de la calculadora

--- gen/com/crz/R.java
+++ gen/com/crz/R.java
@@ -11,34 +11,38 @@ public final class R {
     public static final class attr {
     }
 
+    public static final class drawable {
+        public static final int agricalc_hdpi = 0x7f020000;
+    }
+
     public static final class id {
-        public static final int btnOk = 0x7f04000c;
-        public static final int entry = 0x7f040001;
-        public static final int labelInputArea = 0x7f040000;
-        public static final int output = 0x7f04000d;
-        public static final int rbFromHa = 0x7f040004;
-        public static final int rbFromMts2 = 0x7f040006;
-        public static final int rbFromMz = 0x7f040003;
-        public static final int rbFromVrs2 = 0x7f040005;
-        public static final int rbToHa = 0x7f040009;
-        public static final int rbToMts2 = 0x7f04000b;
-        public static final int rbToMz = 0x7f040008;
-        public static final int rbToVrs2 = 0x7f04000a;
-        public static final int rbgInputUnits = 0x7f040002;
-        public static final int rbgOutputUnits = 0x7f040007;
+        public static final int btnConvert = 0x7f05000c;
+        public static final int labelInputArea = 0x7f050000;
+        public static final int rbFromHa = 0x7f050004;
+        public static final int rbFromMts2 = 0x7f050006;
+        public static final int rbFromMz = 0x7f050003;
+        public static final int rbFromVrs2 = 0x7f050005;
+        public static final int rbToHa = 0x7f050009;
+        public static final int rbToMts2 = 0x7f05000b;
+        public static final int rbToMz = 0x7f050008;
+        public static final int rbToVrs2 = 0x7f05000a;
+        public static final int rbgInputUnits = 0x7f050002;
+        public static final int rbgOutputUnits = 0x7f050007;
+        public static final int txtInputArea = 0x7f050001;
+        public static final int txtOutputArea = 0x7f05000d;
     }
 
     public static final class layout {
-        public static final int main = 0x7f020000;
+        public static final int main = 0x7f030000;
     }
 
     public static final class string {
-        public static final int app_name = 0x7f030000;
-        public static final int buttonConvertText = 0x7f030006;
-        public static final int explanation = 0x7f030001;
-        public static final int inputAreaText = 0x7f030005;
-        public static final int outputAreaText = 0x7f030002;
-        public static final int unitInputAreaText = 0x7f030004;
-        public static final int unitOutputAreaText = 0x7f030003;
+        public static final int app_name = 0x7f040000;
+        public static final int buttonConvertText = 0x7f040006;
+        public static final int explanation = 0x7f040001;
+        public static final int inputAreaText = 0x7f040005;
+        public static final int outputAreaText = 0x7f040002;
+        public static final int unitInputAreaText = 0x7f040004;
+        public static final int unitOutputAreaText = 0x7f040003;
     }
 }
--- src/com/crz/MainActivity.java
+++ src/com/crz/MainActivity.java
@@ -2,8 +2,19 @@ package com.crz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class MainActivity extends Activity {
+
+    private Button btnConvert;
+    private EditText txtInputArea;
+    private RadioGroup rbgInputUnits;
+    private RadioGroup rbgOutputUnits;
+    private EditText txtOutputArea;
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +22,62 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        btnConvert = (Button) findViewById(R.id.btnConvert);
+        txtInputArea = (EditText) findViewById(R.id.txtInputArea);
+        txtOutputArea = (EditText) findViewById(R.id.txtOutputArea);
+        rbgInputUnits = (RadioGroup) findViewById(R.id.rbgInputUnits);
+        rbgOutputUnits = (RadioGroup) findViewById(R.id.rbgOutputUnits);
+
+
+        btnConvert.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Calcular el area en la nueva unidad de area
+                String strInputArea = txtInputArea.getText().toString();
+                double inputArea = Double.parseDouble(strInputArea);
+                double outputArea = 0;
+                int inputUnit = rbgInputUnits.getCheckedRadioButtonId();
+                int outputUnit = rbgOutputUnits.getCheckedRadioButtonId();
+                double inputBase = 1.0;
+                double outputBase = 1.0;
+
+                switch (inputUnit) {
+                    case R.id.rbFromMz:
+                        inputBase = 7000.0;
+                        break;
+                    case R.id.rbFromHa:
+                        inputBase = 10000.0;
+                        break;
+                    case R.id.rbFromVrs2:
+                        inputBase = 0.70;
+                        break;
+                    case R.id.rbFromMts2:
+                        inputBase = 1.0;
+                        break;
+                }
+
+                // Convierte el area ingresada a metros cuadrados
+                inputArea = inputArea * inputBase;
+
+                switch (outputUnit) {
+                    case R.id.rbToMz:
+                        outputBase = 1 / 7000.0;
+                        break;
+                    case R.id.rbToHa:
+                        outputBase = 1 / 10000.0;
+                        break;
+                    case R.id.rbToVrs2:
+                        outputBase = 1 / 0.70;
+                        break;
+                    case R.id.rbToMts2:
+                        outputBase = 1 / 1.0;
+                        break;
+                }
+
+                outputArea = inputArea * outputBase;
+
+                txtOutputArea.setText(Double.toString(outputArea));
+            }
+        });
     }
 }
